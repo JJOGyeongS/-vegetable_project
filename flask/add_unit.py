4805c5811392ccd3953c078d4bb49ac4a5fd1b45
JJOGyeongS <95284588+JJOGyeongS@users.noreply.github.com>
2022-12-10 12:11:13 +0900

    Add files via upload

---
+++ flask/add_unit.py
@@ -0,0 +1,25 @@
+import json
+import os
+import sqlite3
+
+def add_unit(item):
+	conn = sqlite3.connect("C:/Users/jsjs8/veteran-project1/Flask/kamis_data.db")
+	c = conn.cursor()
+	if item['item_name'] != "고등어":
+		c.execute(
+			"SELECT * FROM items_unit "
+			"WHERE item_code=" +str(item['item_code'])
+		)
+		data = c.fetchall()
+		result = {
+		'item_name' : item['item_name'],
+		'unit' : data[0][1],
+		'unit_price' : int(item['item_price'] * data[0][2])
+		}
+	else :
+		result = {
+			'item_name' : item['item_name'],
+			'unit' : item['kind_name'],
+			'unit_price' : item['item_price']
+		}
+	return result
